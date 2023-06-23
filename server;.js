f5751a990be2d4ea1d5b3cbd14c6cba3e61754f1
joel Quezada <halo.joel.quezada@gmail.com>
2023-06-22 20:14:58 -0700

    answers

--- server;.js
+++ server;.js
@@ -28,6 +28,33 @@ function start() {
     'Add a role',
     'Add an employee',
     'Update an employee role',
-    'Exit']
+    'Exit' ]
+  }).then(function(answer) {
+    switch (answer.action) {
+      case 'View all departments':
+        viewDepartments();
+        break;
+      case 'View all roles':
+        viewRoles();
+        break;
+      case 'View all employees':
+        viewEmployees();
+        break;
+      case 'Add a department':
+        addDepartment();
+        break;
+      case 'Add a role':
+        addRole();
+        break;
+      case 'Add an employee':
+        addEmployee();
+        break;
+      case 'Update an employee role':
+        updateEmployeeRole();
+        break;
+      case 'Exit':
+        db.end();
+        break;
+    }
+  });
 }
-)};
