f08def287b34cdeab18b736de95c6cf25ee4f9e9
joel Quezada <halo.joel.quezada@gmail.com>
2023-06-22 19:20:26 -0700

    role

--- schema.sql
+++ schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE department (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
