94da148dec864643b844f3ea1aa6c8d20f11d8fb
joel Quezada <halo.joel.quezada@gmail.com>
2023-06-22 20:12:56 -0700

    choices

--- server;.js
+++ server;.js
@@ -20,6 +20,14 @@ function start() {
     name: 'action',
     type: 'list',
     message: 'What would you like to do?',
-    choices: []
+    choices: [
+    'View all departments',
+    'View all roles',
+    'View all employees',
+    'Add a department',
+    'Add a role',
+    'Add an employee',
+    'Update an employee role',
+    'Exit']
 }
 )};
